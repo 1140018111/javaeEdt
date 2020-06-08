98d5270279e51f303790587802c8a1210936dda7
1140018111@qq.com <1140018111@qq.com>
2020-06-08 23:14:39 +0800

    20200608 Filter拦截器

--- src/main/java/com/DxlApplication.java
+++ src/main/java/com/DxlApplication.java
@@ -3,11 +3,14 @@ package com;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @MapperScan(basePackages="com.generator.dao")
 @ImportResource(locations = {"classpath:spring-mvc.xml"})
+//拦截器注解
+@ServletComponentScan
 public class DxlApplication {
 
 
---
+++ src/main/java/com/filter/FilterTest.java
@@ -0,0 +1,30 @@
+package com.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+/**
+ * com.filter
+ * dxl
+ *
+ * @author DL
+ * @create 2020-06-08
+ */
+@WebFilter(filterName = "用户拦截器",urlPatterns = "/query/a")
+public class FilterTest implements javax.servlet.Filter {
+	@Override
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+		System.out.println("拦截器执行方法。");
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		System.out.println("拦截器初始化方法");
+	}
+
+	@Override
+	public void destroy() {
+		System.out.println("过滤销毁方法");
+	}
+}
